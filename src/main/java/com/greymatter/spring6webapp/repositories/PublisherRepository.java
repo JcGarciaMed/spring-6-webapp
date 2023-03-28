bbbaa22a39f74180157da023dfa225c8d3735a28
Jorge García <jcgarciamed@gmail.com>
2023-03-27 22:01:20 -0500

    Publisher

---
+++ src/main/java/com/greymatter/spring6webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.greymatter.spring6webapp.repositories;
+
+import com.greymatter.spring6webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
