5333fc545844a7fd99d2960dba0f8a355ffad4d2
Jorge García <jcgarciamed@gmail.com>
2023-03-28 09:06:55 -0500

    Create Service Layer

---
+++ src/main/java/com/greymatter/spring6webapp/services/BookService.java
@@ -0,0 +1,8 @@
+package com.greymatter.spring6webapp.services;
+
+import com.greymatter.spring6webapp.domain.Book;
+
+public interface BookService {
+
+    Iterable<Book> findAll();
+}
---
+++ src/main/java/com/greymatter/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,19 @@
+package com.greymatter.spring6webapp.services;
+
+import com.greymatter.spring6webapp.domain.Book;
+import com.greymatter.spring6webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
